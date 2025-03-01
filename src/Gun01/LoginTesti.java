3738ea45ddf3d5509ea51f7f29dc6500a7662286
Selcuk <saityldrimm@gmail.com>
2025-03-01 16:39:25 +0100

    Logın testi asama 1(jiradaki gelen task daki baslık)

---
+++ src/Gun01/LoginTesti.java
@@ -0,0 +1,14 @@
+package Gun01;
+
+public class LoginTesti {
+    public static void main(String[] args) {
+
+
+        // Bu bolum Login testi icin , yeni sifre modeline gore yazıldı.
+        System.out.println("Yeni login testi ");
+
+        // cok satur yazdım kodları bitirdim.
+        System.out.println("Login TEsti 1");
+
+    }
+}
